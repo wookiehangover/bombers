7d2be1ee45cef88e651952cec19b1f6aca9924f8
Sam Breed <sam@quickleft.com>
2011-12-26 00:18:28 -0700

    more work on routing to pages

--- app/modules/page.js
+++ app/modules/page.js
@@ -11,19 +11,28 @@ module.exports = Backbone.View.extend({
     this.bind( 'show', this.onShow, this );
     this.bind( 'hide', this.onHide, this );
 
-    //Bombers.app.router.route( this.id, this.id, function( id ){
-      //console.log(id);
-    //});
   },
 
   onShow: function(){
     this.el.addClass('ui-active');
-    Bombers.app.router.navigate( this.route );
+    this.navigate();
   },
 
   onHide: function(){
     this.el.removeClass('ui-active');
+  },
+
+  show: function(){
+    var
+      current = $('.ui-active').index(),
+      speed   = current === this.index ? 600 : 600 * this.index;
+
+    $('html,body').stop().animate({ scrollTop: this.el.data('height') }, speed, 'easeInOutQuad' );
+  },
+
+  navigate: function(){
+    if( $('html,body').is(':animated') === false ){
+      Bombers.app.router.navigate( this.route, false );
+    }
   }
-
-
 });
